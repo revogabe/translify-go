6781ada20321251c78dc5429795890dd8425717c
Daniel Gabriel <reelpscanal@gmail.com>
2023-05-04 13:08:21 -0300

    commit add route delete

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { PlusCircledIcon } from '@radix-ui/react-icons'
-import { useQuery } from '@tanstack/react-query'
+import { PlusCircledIcon, TrashIcon } from '@radix-ui/react-icons'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
@@ -14,8 +14,13 @@ type TopicsProps = {
   userId: string
 }
 
+type SystemProps = {
+  systemId: string
+}
+
 export const SideBar = () => {
   const { data: session } = useSession()
+  const queryClient = useQueryClient()
 
   const { data } = useQuery({
     queryKey: ['topics'],
@@ -32,6 +37,25 @@ export const SideBar = () => {
     },
     enabled: !!session?.user,
   })
+
+  const mutate = useMutation({
+    mutationFn: async ({ data }: { data: SystemProps }) => {
+      const response = await axios.delete<SystemProps>(
+        'http://localhost:3000/api/delete-topics?systemId=' + data.systemId,
+        {
+          headers: {
+            Authorization: session?.user?.email,
+          },
+        },
+      )
+
+      return response.data
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries(['topics'])
+    },
+  })
   return (
     <div className="flex min-h-screen w-[420px] flex-col gap-6 bg-zinc-800 p-6">
       <Link
@@ -40,17 +64,27 @@ export const SideBar = () => {
       >
         <PlusCircledIcon width={20} height={20} /> New Chat
       </Link>
-      <ul className="flex w-full flex-col gap-2">
+      <div className="flex w-full flex-col gap-2">
         {data?.map((topic: TopicsProps) => (
-          <Link
-            href={`/chat/${topic.id}`}
-            className="flex items-center justify-center gap-2 rounded-lg bg-zinc-700/50 p-4 font-medium text-zinc-300 duration-150 ease-out hover:bg-zinc-700 hover:text-white active:scale-95"
+          <div
             key={topic.id}
+            className="flex items-center justify-between gap-2 rounded-lg bg-zinc-700/50 font-medium text-zinc-300 duration-150 ease-out hover:bg-zinc-700 hover:text-white active:scale-95"
           >
-            {topic.title}
-          </Link>
+            <Link
+              className="flex h-full w-full items-center justify-start p-6"
+              href={`/chat/${topic.id}`}
+            >
+              {topic.title}
+            </Link>
+            <TrashIcon
+              onClick={() => mutate.mutate({ data: { systemId: topic.id } })}
+              className="mr-3 cursor-pointer rounded-lg p-2 text-zinc-500 duration-150 ease-out hover:bg-zinc-900/50 hover:text-red-500"
+              width={42}
+              height={42}
+            />
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
---
+++ src/pages/api/delete-topics.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { db } from '@/lib/db'
+import { TMessageRequest } from '@/types'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'DELETE') {
+    try {
+      const token = req.headers.authorization
+      const query = req.query as TMessageRequest
+
+      if (!token) {
+        return res.status(401).end()
+      }
+
+      const findUser = await db.user.findUnique({
+        where: {
+          email: token,
+        },
+      })
+
+      if (!findUser) {
+        return res.status(401).end()
+      }
+
+      const deleteSystem = await db.system.delete({
+        where: {
+          id: query.systemId,
+        },
+      })
+
+      if (!deleteSystem) {
+        return res.status(404).send({ message: 'System not found' })
+      }
+
+      return res.status(201).send(deleteSystem)
+    } catch (error) {
+      return res.status(422).end()
+    }
+  }
+}
