1c2368901a8c103574e6185c86238f72415612ce
Daniel Gabriel <reelpscanal@gmail.com>
2023-05-09 00:13:00 -0300

    remove google button

--- src/components/ContainerLogin.tsx
+++ src/components/ContainerLogin.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { BoxIcon, GitHubLogoIcon } from '@radix-ui/react-icons'
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
 import { signIn } from 'next-auth/react'
 
 export const ContainerLogin = () => {
@@ -10,17 +10,11 @@ export const ContainerLogin = () => {
     })
   }
 
-  function handleLoginGoogle() {
-    signIn('google', {
-      callbackUrl: '/',
-    })
-  }
-
-  function handleLoginApple() {
-    signIn('apple', {
-      callbackUrl: '/',
-    })
-  }
+  // function handleLoginGoogle() {
+  //   signIn('google', {
+  //     callbackUrl: '/',
+  //   })
+  // }
 
   return (
     <div className="flex w-full items-center justify-start gap-4">
@@ -31,20 +25,13 @@ export const ContainerLogin = () => {
         <GitHubLogoIcon className="h-6 w-6" />
         <p>GitHub</p>
       </button>
-      <button
+      {/* <button
         onClick={handleLoginGoogle}
         className="flex w-max cursor-pointer items-center justify-center gap-3 rounded-lg border border-zinc-800 bg-zinc-900 px-8 py-6 text-lg font-bold text-zinc-200 duration-200 ease-out  hover:bg-emerald-500 hover:text-zinc-900 active:scale-95  max-sm:px-6 max-sm:py-4 max-sm:text-sm"
       >
         <BoxIcon className="h-6 w-6" />
         <p>Google</p>
-      </button>
-      <button
-        onClick={handleLoginApple}
-        className="flex w-max cursor-pointer items-center justify-center gap-3 rounded-lg border border-zinc-800 bg-zinc-900 px-8 py-6 text-lg font-bold text-zinc-200 duration-200 ease-out  hover:bg-emerald-500 hover:text-zinc-900 active:scale-95  max-sm:px-6 max-sm:py-4 max-sm:text-sm"
-      >
-        <BoxIcon className="h-6 w-6" />
-        <p>Apple</p>
-      </button>
+      </button> */}
     </div>
   )
 }
